c14fac6fb239241a4f8f435c40b71f21d1bae304
abhi <abhirathdubey2804@gmail.com>
2022-11-27 16:26:01 +0530

    commit

--- cudderproject/settings.py
+++ cudderproject/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
