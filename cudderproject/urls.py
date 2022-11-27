d812037f97c7e9dbe4c61c002271cb958a6e0f09
abhi <abhirathdubey2804@gmail.com>
2022-11-27 16:45:12 +0530

    commit

--- cudderproject/urls.py
+++ cudderproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
 
 from django.views.static import serve
 from django.urls import re_path as url
